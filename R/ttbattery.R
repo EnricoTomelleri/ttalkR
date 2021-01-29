8c0222bb2cf9b864e09ee166fffd2f74eb929cf1
EnricoTomelleri <etomelleri@unibz.it>
2021-01-29 14:05:28 +0100

    ttbattery, ttgranier and ttscrape

---
+++ R/ttbattery.R
@@ -0,0 +1,47 @@
+ttbattery <- function(mydata_4B, mydata_4D, plot_label){
+
+  HR_Timestamp_4D <- as.POSIXct(mydata_4D$Timestamp, origin="1970-01-01")
+  HR_Timestamp_4B <- as.POSIXct(mydata_4B$Timestamp, origin="1970-01-01")
+  #create a color index
+  id_col <- mydata_4D$IT_ID
+  id_col[id_col == max(id_col, na.rm=T)] <- 21
+  id_col[id_col != 21] <- id_col[id_col != 21]- max(id_col, na.rm=T)
+  mydata_4D$id_col <- abs(id_col)
+
+
+#4.8.Battery Voltage
+Bat_mV <- 2*1100*(mydata_4D$adc_Vbat/mydata_4D$adc_bandgap)
+#jpeg(paste("../Figures/Bat_mV_", site, ".jpg", sep=""), width = 1200, height = 800)
+#plot(mydata_4D$Timestamp, Bat_mV, pch=20, ylim=c(3300,4500), xlim=c(min(mydata_4D$Timestamp, na.rm=T),max(mydata_4D$Timestamp, na.rm=T)), main=site, xlab="Timestamp")
+#segments(min(mydata_4D$Timestamp, na.rm=T),3500,max(mydata_4D$Timestamp, na.rm=T),3500, col="red")
+#lines( mydata_4B$Timestamp, mydata_4B$Battery, col="cyan")
+#col_ind <- mydata_4D$Timestamp-min(mydata_4D$Timestamp)
+#plot(HR_Timestamp_4D, Bat_mV, pch=20, ylim=c(3300,4500), xlim=c(min(HR_Timestamp_4D, na.rm=T),max(HR_Timestamp_4D, na.rm=T)), main=site, xlab="Timestamp", col=topo.colors(21)[id_col])
+#segments(min(HR_Timestamp_4D, na.rm=T),3500,max(HR_Timestamp_4D, na.rm=T),3500, col="red")
+#lines(HR_Timestamp_4B, mydata_4B$Battery, col="cyan")
+#segments(15500,0,15500,5000, col="red")
+#dev.off()
+
+
+
+
+library(ggplot2)
+df <- data.frame(HR_Timestamp_4D, Bat_mV, id_col)
+df1 <- data.frame(HR_Timestamp_4B, mydata_4B$Battery); colnames(df1) <- c("HR_Timestamp_4B", "Bat_mV")
+p <- ggplot(data=df, aes(HR_Timestamp_4D, Bat_mV))
+p + geom_point(aes(colour = id_col), size = 0.2) +
+  scale_color_gradientn(colours = hcl.colors(21, palette = "viridis")) +
+  geom_line(data=df1, aes(HR_Timestamp_4B, Bat_mV), color = "black") +
+  geom_segment(aes(x = min(HR_Timestamp_4D, na.rm=T), y = 3500, xend = max(HR_Timestamp_4D, na.rm=T), yend = 3500), color = "red") +
+  labs(x = "Timestamp") +
+  #labs(title = site) +
+  scale_x_datetime(minor_breaks=("1 week")) +
+  theme(legend.position = "none")
+#save the plot
+#ggsave(paste("../Figures/", site, "_BatteryVoltage.png", sep=""),
+#       plot = last_plot(),
+#       width = 10,
+#       height = 7,
+#       units = c("in"),
+#       dpi = 300)
+}
---
+++ R/ttscrape.R
@@ -0,0 +1,74 @@
+ttscrape <- function(ID){
+  #Specifying the url for desired website to be scraped
+  url <- paste("http://naturetalkers.altervista.org/", ID, "/ttcloud.txt", sep="")
+  #Reading the HTML code from the website
+  mydata0 <- read.csv(url, sep=";", header=FALSE, fill=TRUE)
+
+
+  #Specifying the url for desired website to be scraped
+  url <- paste("http://ittn.altervista.org/", ID, "/ttcloud.txt", sep="")
+  #Reading the HTML code from the website
+  mydata1 <- read.csv(url, sep=";", header=FALSE, fill=TRUE)
+
+  mydata <- rbind(mydata0, mydata1)
+
+
+  mydata_sep <- separate(mydata, V1, into=c("Date", "Time", "SN"), sep = "[ ,]")
+
+  #split the dataset
+  mydata_4D <- mydata_sep[mydata_sep$V3 == "4D",]
+
+  mydata_49 <- mydata_sep[mydata_sep$V3 == "49",]
+
+  mydata_4B <- mydata_sep[mydata_sep$V3 == "4B",]
+
+  mydata_4C <- mydata_sep[mydata_sep$V3 == "4C",]
+
+
+  header_4D <- c("SDate", "STime", "IT_ID", "Rec_Nr", "Dev_Ty", "Timestamp",
+                 "Tref_0", "Theat_0", "growt_sens", "adc_bandgap", "Bits", "RH",
+                 "Tair", "gz_mean", "gz_sd", "gy_mean", "gy_sd", "gx_mean", "gx_sd",
+                 "Tref_1", "Theat_1", "StWC", "adc_Vbat")
+  header_49 <- c("SDate", "STime", "IT_ID", "Rec_Nr", "Dev_Ty", "Timestamp",
+                 "AS7263_610", "AS7263_680", "AS7263_730", "AS7263_760",
+                 "AS7263_810", "AS7263_860", "AS7263_450", "AS7263_500",
+                 "AS7263_550", "AS7263_570", "AS7263_600", "AS7263_650",
+                 "integration_T", "gain")
+
+  header_4B <- c("SDate", "STime", "IT_ID", "Rec_Nr", "Dev_Ty", "Timestamp",
+                 "Acc_Recs", "Recs_to_be_sent", "MCC_tel_op", "MNC_tel_op",
+                 "GSM_reg", "GSM_field", "Battery", "Firmware_ver")
+
+  header_4C <-  c("SDate", "STime", "IT_ID", "Rec_Nr", "Dev_Ty", "Timestamp",
+                  "TBL_locked", "n_first_sens", "RSSI_TT1", "RSSI_TT2", "RSSI_TT3",
+                  "RSSI_TT4", "RSSI_TT5", "RSSI_TT6", "RSSI_TT7", "RSSI_TT8", "RSSI_TT9",
+                  "RSSI_TT10", "RSSI_TT11", "RSSI_TT12", "RSSI_TT13", "RSSI_TT14", "RSSI_TT15",
+                  "RSSI_TT16", "RSSI_TT17", "RSSI_TT18", "RSSI_TT19", "RSSI_TT20")
+
+  colnames(mydata_4D) <- header_4D
+
+  colnames(mydata_49) <- header_49
+
+  colnames(mydata_4B) <- header_4B
+
+  colnames(mydata_4C) <- header_4C
+
+  mydata_4D$Timestamp[mydata_4D$Timestamp < 2000000] <- NA
+
+  mydata_4D$IT_ID <- as.integer(mydata_4D$IT_ID)
+  mydata_4D$gz_mean <- as.integer(mydata_4D$gz_mean)
+
+
+  #timestamp conversion
+  HR_Timestamp_4D <- as.POSIXct(mydata_4D$Timestamp, origin="1970-01-01")
+  HR_Timestamp_4B <- as.POSIXct(mydata_4B$Timestamp, origin="1970-01-01")
+  #create a color index
+  id_col <- mydata_4D$IT_ID
+  id_col[id_col == max(id_col, na.rm=T)] <- 21
+  id_col[id_col != 21] <- id_col[id_col != 21]- max(id_col, na.rm=T)
+  mydata_4D$id_col <- abs(id_col)
+
+  mydata_4B <<- mydata_4B
+  mydata_4D <<- mydata_4D
+
+}
