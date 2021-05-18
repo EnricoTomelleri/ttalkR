730c5ab19f3029bdcc4108123e03c9d0b857cc4a
EnricoTomelleri <etomelleri@unibz.it>
2021-05-18 21:13:09 +0200

    Update ttstability.R

--- R/ttstability.R
+++ R/ttstability.R
@@ -38,11 +38,12 @@ ttstability <- function(mydata_4D, plot_label){
 
 
 
-  df1 <- data.frame(HR_Timestamp_4D, phi); colnames(df1) <- c("HR_Timestamp_4B", "phi")
+  df1 <- data.frame(mydata_4D$Timestamp, phi, mydata_4D$id_col_ind)
+  colnames(df1) <- c("Timestamp", "phi", "id_col_ind")
 
   if (plot_label == "all_in_one"){
-    p <- ggplot(data=df1, aes(HR_Timestamp_4D, phi)) +
-      geom_point(aes(colour = mydata_4D$id_col_ind), size = 0.2) +
+    p <- ggplot(data=df1, aes(Timestamp, phi)) +
+      geom_point(aes(colour = id_col_ind), size = 0.2) +
       scale_color_gradientn(colours = hcl.colors(30, palette = "viridis")) +
       labs(x = "Timestamp", y = "phi (degrees)") +
       #labs(title = site) +
@@ -57,8 +58,8 @@ ttstability <- function(mydata_4D, plot_label){
 
 
   if (plot_label == "split"){
-    p <- ggplot(data=df1, aes(x = HR_Timestamp_4D, y = phi, color=mydata_4D$id_col_ind)) +
-      geom_point(aes(colour = mydata_4D$id_col_ind), size = 0.2)  +
+    p <- ggplot(data=df1, aes(x = Timestamp, y = phi, color=id_col_ind)) +
+      geom_point(aes(colour = id_col_ind), size = 0.2)  +
       scale_color_gradientn(colours = hcl.colors(30, palette = "viridis")) +
       labs(x = "Timestamp", y = "phi (degrees)") +
       theme(legend.position = "none") +
@@ -68,9 +69,9 @@ ttstability <- function(mydata_4D, plot_label){
       theme(strip.text.y = element_text(angle = 0, hjust = 0)) +
       ylim(-20, 20)+
       geom_segment(aes(
-        x = min(HR_Timestamp_4D, na.rm = T),
+        x = min(Timestamp, na.rm = T),
         y = 0,
-        xend = max(HR_Timestamp_4D, na.rm = T),
+        xend = max(Timestamp, na.rm = T),
         yend = 0
       ), color = "red")
 
@@ -83,71 +84,3 @@ ttstability <- function(mydata_4D, plot_label){
 
 
 
-
-
-ttmovement <- function(mydata_4D, plot_label){
-
-  #load required packages
-  library(ggplot2)
-
-
-  #create a color index
-  id_col <- mydata_4D$TT_ID
-  id_col_ind <- data.frame(unique(id_col), 1:length(unique(id_col))); colnames(id_col_ind) <- c("TT_ID", "ID")
-  #create index for color scale
-  mydata_4D$id_col_ind <- mydata_4D$TT_ID
-  for (i in 1:length(id_col_ind$ID)){
-    mydata_4D$id_col_ind <- replace(mydata_4D$id_col_ind, mydata_4D$id_col_ind==id_col_ind$TT_ID[i], id_col_ind$ID[i])
-  }
-
-
-
-  #trunk_axis_movement
-  Dx <- mydata_4D$gx_sd
-  Dy <- mydata_4D$gy_sd
-  Dz <- mydata_4D$gz_sd
-
-  tam <- Dx + Dy + Dz
-
-
-  df1 <- data.frame(HR_Timestamp_4D, tam); colnames(df1) <- c("HR_Timestamp_4B", "tam")
-
-
-  if (plot_label == "all_in_one"){
-    p <- ggplot(data=df1, aes(HR_Timestamp_4D, tam)) +
-      geom_point(aes(colour = mydata_4D$id_col_ind), size = 0.2) +
-      scale_color_gradientn(colours = hcl.colors(30, palette = "viridis")) +
-      labs(x = "Timestamp", y = "phi (degrees)") +
-      #labs(title = site) +
-      theme(legend.position = "none") +
-      scale_x_datetime(minor_breaks=("1 week")) #+
-    ylim(-0,100)
-    print(p)
-
-  }
-
-
-  if (plot_label == "split"){
-    p <- ggplot(data=df1, aes(x = HR_Timestamp_4D, y = phi, color=mydata_4D$id_col_ind)) +
-      geom_point(aes(colour = mydata_4D$id_col_ind), size = 0.2)  +
-      scale_color_gradientn(colours = hcl.colors(30, palette = "viridis")) +
-      labs(x = "Timestamp", y = "phi (degrees)") +
-      theme(legend.position = "none") +
-      scale_x_datetime(minor_breaks=("1 week")) +
-      facet_grid(facets = mydata_4D$TT_ID ~ ., margins = FALSE) +
-      theme(legend.position = "none") +
-      theme(strip.text.y = element_text(angle = 0, hjust = 0)) +
-      ylim(-20, 20)+
-      geom_segment(aes(
-        x = min(HR_Timestamp_4D, na.rm = T),
-        y = 0,
-        xend = max(HR_Timestamp_4D, na.rm = T),
-        yend = 0
-      ), color = "red")
-
-    print(p)
-  }
-
-  if (plot_label == "none"){}
-
-}
