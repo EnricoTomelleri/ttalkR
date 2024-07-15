4296cf6e895a170faa8f22be8abb00e734a62efe
Enrico Tomelleri <68595481+EnricoTomelleri@users.noreply.github.com>
2024-07-15 15:11:08 +0200

    Update ttScrape_adjusted_v2.1.R

--- R/ttScrape_adjusted_v2.1.R
+++ R/ttScrape_adjusted_v2.1.R
@@ -63,8 +63,8 @@ ttScrape_v2 <- function (ID, subset_days)
     mydata_4D <- mydata_4D[mydata_4D$SN > 5.2e+07, ]
     mydata_4D <- Filter(function(x) !all(is.na(x)), mydata_4D)
     mydata_4D <- mydata_4D %>% mutate_if(bit64::is.integer64,
-                                         as.integer)
-    mydata_49 <- mydata_sep[mydata_sep$V3 == "49", ]
+                                         as.numeric)
+        mydata_49 <- mydata_sep[mydata_sep$V3 == "49", ]
     mydata_49 <- mydata_49[mydata_49$SN > 5.2e+07, ]
     mydata_49 <- Filter(function(x) !all(is.na(x)), mydata_49)
     mydata_49$V5 <- as.numeric(mydata_49$V5)
@@ -108,8 +108,8 @@ ttScrape_v2 <- function (ID, subset_days)
     colnames(mydata_4B) <- header_4B
     header_4C <- header_4C[1:(dim(mydata_4C)[2])]
     colnames(mydata_4C) <- header_4C
+    mydata_4D$Timestamp <- as.integer(mydata_4D$Timestamp)
     mydata_4D$Timestamp[mydata_4D$Timestamp < 1577836800] <- NA
-    mydata_4D$Timestamp <- as.numeric(as.character(mydata_4D$Timestamp))
     mydata_4D$TT_ID <- as.integer(mydata_4D$TT_ID)
     mydata_49$TT_ID <- as.integer(mydata_49$TT_ID)
     mydata_4B$TT_ID <- substr(mydata_4B$TT_ID, 2, 8)
